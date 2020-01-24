6d88bc819a8e43396fc94d5e0c10070518cbef07
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-01-24 17:18:20 +0000

    wonTimes and oneRoll

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //random number variable between 1 and 6
 
-
 //html element declarations
 const rollButton = document.getElementById("roll");
 const playerScore = document.getElementById("playerScore");
 const diceImage = document.getElementById("diceImage");
 const totalScore = document.getElementById("totalScore")
 const message = document.getElementById("message")
-
+const wonTimes = document.getElementById("wonTimes")
+const oneRoll = document.getElementById("oneRoll")
 
 // consts go here
 
@@ -15,6 +15,8 @@ diceImage.style.visibility = "hidden";
 
 let numberRolled;
 let pointsScored = 0;
+let wonTimes1 = 0;
+let oneRoll1 = 0;
 
 //functions declaration
 
@@ -23,8 +25,6 @@ const diceRoll = () => {
     console.log(`   I am the roll variable ${numberRolled}`)
 }
 
-
-
 const winOrLose = () => {
     // numberRolled = 2;
     // pointsScored = 21;
@@ -32,10 +32,18 @@ const winOrLose = () => {
     if (numberRolled == 1) {
         message.textContent = "Try again, you lost!"
         pointsScored = 0;
+        oneRoll1++
+        oneRoll.textContent = `${oneRoll1}`
+        console.log(oneRoll1);
+        
         //resetFunct();
     } else if (pointsScored >= 20) {
         message.textContent = "How did you win? Did you Cheat?"
         pointsScored = 0;
+        wonTimes1++
+        wonTimes.textContent = `${wonTimes1}`
+        console.log(wonTimes1);
+        
         //resetFunct();
     } else {
         message.textContent = "Oh, carry on then"
@@ -49,7 +57,6 @@ const scoreAddition = () => {
     //scoreAddition.textContent = (`   My total score is: ${pointsScored}`);   
     //scoreAddition.textContent = "score addition"
 }
-
 
 
 rollButton.addEventListener("click", () => {
@@ -67,6 +74,8 @@ const resetFunct = () => {
     pointsScored = 0;
     playerScore.textContent = 0
     totalScore.textContent = 0
+
+   
     diceImage.style.visibility = "hidden";
     message.textContent = "Start again"
 
@@ -78,10 +87,6 @@ resetBtn.addEventListener("click", () => {
 })
 
 // diceRoll()
-// winOrLose()
-// winOrLose()
-// winOrLose()
-// winOrLose()
 // winOrLose()
 
 // diceRoll()
